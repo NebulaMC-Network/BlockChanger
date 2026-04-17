3c25d4c157bc8c7760e3176533baf55b258da1cc
jsexp <108526035+hardcorefactions@users.noreply.github.com>
2026-04-17 22:44:35 +0200

    feat(build): update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   `java-library`
-  id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
+  id("io.papermc.paperweight.userdev") version "2.0.0-beta.21"
   id("maven-publish")
 }
 
@@ -14,8 +14,8 @@ java {
 
 dependencies {
   paperweight.paperDevBundle("1.21.11-R0.1-SNAPSHOT")
-  implementation("net.bytebuddy:byte-buddy:1.14.3")
-  implementation("net.bytebuddy:byte-buddy-agent:1.17.8")
+  implementation("net.bytebuddy:byte-buddy:1.18.8")
+  implementation("net.bytebuddy:byte-buddy-agent:1.18.8")
 }
 
 paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
